cc839dcb76489ced5287908537c87e45f2449d64
youasl <yousefaslami@outlook.com>
2026-02-27 20:28:31 +0100

    refactor: clean up routes and remove unused CropDiseasesController references

--- routes/api.php
+++ routes/api.php
@@ -3,24 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CropsController;
-use App\Http\Controllers\CropsDiseasesController;
 
 // Fallback on unknown requests
-Route::fallback(function() {
-    $result = [
-        'code' => 404,
-        'status' => 'Not found',
-    ];
-    return response()->json($result, 404);
-});
-
-//These routes handle CRUD operations for crops and crop diseases.
-Route::post('/crops/diseases', [CropsDiseasesController::class, 'store']);
-Route::get('/crops/diseases', [CropsDiseasesController::class, 'index']);
-Route::get('/crops/diseases/{id}', [CropsDiseasesController::class, 'show']);
-Route::patch('/crops/diseases/{id}', [CropsDiseasesController::class, 'update']);
-
-Route::post('/crops', [CropsController::class, 'store']);
-Route::get('/crops', [CropsController::class, 'index']);
-Route::get('/crops/{id}', [CropsController::class, 'show']);
-Route::patch('/crops/{id}', [CropsController::class, 'update']);
+Route::get('/movie-list', [CropsController::class, 'index']);
